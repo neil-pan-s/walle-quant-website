a1a51058a0b607da5a96d1cb3986d60f3e2c7931
neil.pan <neil.pan@qq.com>
2020-12-05 02:27:01 +0800

    :)

---
+++ js/chunk-2d0c938a.1eb5b52b.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c938a"],{"57e7":function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"frame"},[n("ui-nav"),n("router-view"),n("ui-footer")],1)},r=[],u={},c=u,i=n("2877"),s=Object(i["a"])(c,a,r,!1,null,"08832edf",null);t["default"]=s.exports}}]);
